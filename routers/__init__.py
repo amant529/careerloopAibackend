64e659f48a550bb4884036189c0db561fe7c18fc
amant529 <amant0931@gmail.com>
2025-11-19 02:18:26 +0530

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,1 +1,6 @@
+from .builder import router as builder_router
+from .auth import router as auth_router
+from .screen import router as screen_router
+from .admin import router as admin_router
+from .analytics import router as analytics_router
 

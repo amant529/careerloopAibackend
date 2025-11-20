af4a246d72bfde76647f6c774d18597f17a9c3e6
amant529 <amant0931@gmail.com>
2025-11-20 16:36:08 +0530

    Delete routers directory

--- routers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .builder import router as builder_router
-from .auth import router as auth_router
-from .screen import router as screen_router
-from .admin import router as admin_router
-from .analytics import router as analytics_router
-
